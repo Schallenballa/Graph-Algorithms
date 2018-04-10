12f42c4853ebec8d17e6fa016b000d538f8d8b46
Zachary Schallenberger <zachietaylor@msn.com>
2018-04-10 11:58:27 -0600

    Add files via upload

---
+++ assignment3-Final/src/graphalgorithms/GraphAlgorithms.java
@@ -0,0 +1,99 @@
+package graphalgorithms;
+
+import java.io.*;
+import java.util.*;
+
+//Authors: Ahmed Naji, Robert Murch and Zach Schallenberger
+//
+//Date submitted: April x, 2018
+//
+//The following program takes in data from a csv file the form of which can be seen below.
+//It then creates minimum spanning tree's based off of the input using three diffrent methods.
+//
+//Current input:
+//
+//0,1,2,3,4
+//----------
+//∞,6,∞,∞,2
+//6,∞,4,9,6
+//∞,4,∞,8,∞
+//∞,9,8,∞,7
+//2,6,∞,7,∞
+//
+//Note, the input can be altered by altering the text file located in the input folder.
+
+
+public class GraphAlgorithms {
+
+	public static void main(String[] args) throws IOException {
+
+		String inputFile = "src/input/input.txt";
+		String fName = inputFile;
+		String thisLine;
+		String[][] data = new String[100][100];
+		int count = 0;
+		int size = 0;
+
+		PrimsAlgorithm graph = new PrimsAlgorithm();
+		KruskalAlgorithm kGraph = new KruskalAlgorithm();
+		floydWarshall fWGraph = new floydWarshall();
+
+		FileInputStream geek = new FileInputStream(fName);
+		BufferedReader in = new BufferedReader(new InputStreamReader(geek, "UTF-16"));
+
+		while ((thisLine = in.readLine()) != null) {
+
+			String[] array = thisLine.split(",");
+			System.arraycopy(array, 0, data[count], 0, array.length);
+			size = array.length;
+			count++;
+		}
+
+		String[] vert = new String[size];
+		System.arraycopy(data[0], 0, vert, 0, size);
+
+		String[][] arrayData = new String[size][size];
+		for (int i = 0; i < size; i++) {
+			System.arraycopy(data[i + 1], 0, arrayData[i], 0, size);
+		}
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please select the algorithm you wish to run: "
+				+ "1 for Prim's algorithm, 2 for Kruskal's algorithm"
+				+ "or 3 for Floyd-Warshall’s algorithm ");
+
+		while (sc.hasNext()) {
+
+			int i = sc.nextInt();
+			switch (i) {
+
+			case 1:
+
+				System.out.println();
+				System.out.println("You chose Prim's algorithm.");
+				graph.initializeData(arrayData, vert);
+				break;
+
+			case 2:
+				System.out.println();
+				System.out.println("You chose Kruskal's algorithm.");
+				kGraph.initializeData(arrayData, vert);
+				break;
+
+			case 3:
+				System.out.println();
+				System.out.println("You chose Floyd-Warshall's algorithm.");
+				fWGraph.initializeData(arrayData, vert);
+				break;
+
+			default:
+				System.out.println("Invalid entry");
+				break;
+			}
+			System.out.println();
+			System.out.println("Please select the algorithm you wish to run: "
+					+ "1 for Prim's algorithm, 2 for Kruskal's algorithm"
+					+ "or 3 for Floyd-Warshall’s algorithm ");
+		}
+	}
+}
---
+++ assignment3-Final/src/graphalgorithms/KruskalAlgorithm.java
@@ -0,0 +1,114 @@
+package graphalgorithms;
+
+import java.util.Arrays;
+
+public class KruskalAlgorithm {
+
+    String[] verticies;
+    int[][] actualData;
+    int max = Integer.MAX_VALUE;
+    int numVerticies;
+
+    //Construct the data array:
+    public void initializeData(String[][] incomingArray, String [] vert) {
+        
+        verticies = vert;
+        actualData = new int[incomingArray.length][incomingArray.length];
+
+        for (int i = 0; i < actualData.length; i++) {
+            for (int j = 0; j < incomingArray.length; j++) {
+
+                if (incomingArray[i][j].equals("\u221E")) {
+
+                    actualData[i][j] = 0;      //if inf symbol is seen replace with high value weight
+                } else {
+                    actualData[i][j] = Integer.parseInt(incomingArray[i][j]);
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println("The adjacency matrix you are finding the MST for is: ");
+        for (int[] actualData1 : actualData) {
+            for (int j = 0; j < actualData1.length; j++) {
+                System.out.print("\t" + actualData1[j]);
+            }
+            System.out.println();
+        }
+
+        numVerticies = actualData.length;
+        kruskalAlg(actualData);
+    }
+
+    //Print the MST:
+    void print(int parent[], int n, int graph[][]) {
+        
+        System.out.println();
+        System.out.println("Verticies: ");
+        System.out.println(Arrays.toString(verticies));
+        System.out.println("------------------------");
+        System.out.println();
+        System.out.println("MST Edge's: ");
+        for (int i = 1; i < numVerticies; i++) {
+            System.out.println(((parent[i]+2)/2) + "---" + i);
+        }
+    }
+
+    //Find's minimum vertex in MST:
+    int minimumVertex(int key[], Boolean[] mstArray) {
+
+        int min = max;
+        int smallestIndex = -1;
+
+        for (int i = 0; i < numVerticies; i++) {
+            if (mstArray[i] == false) {
+                if (key[i] < min) {
+                    min = key[i];
+                    smallestIndex = i;
+                }
+            }
+        }
+
+        return smallestIndex;
+    }
+
+    //Builds MST:
+    void kruskalAlg(int graph[][]) {
+
+        int[] arrayForMST = new int[numVerticies];
+        int[] minWeight = new int[numVerticies];
+        Boolean mstSet[] = new Boolean[numVerticies];
+
+        //Initialize minWeight as large number
+        for (int i = 0; i < numVerticies; i++) {
+            minWeight[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
+        //Not directional therefore initialize:
+        minWeight[0] = 0;
+        arrayForMST[0] = -1;
+
+        for (int count = 0; count < numVerticies - 1; count++) {
+
+            int a = minimumVertex(minWeight, mstSet);
+
+            //Add vertex:
+            mstSet[a] = true;
+
+            //Update value:
+            for (int v = 0; v < numVerticies; v++) {
+                if (graph[a][v] != 0 && mstSet[v] == false) {
+                    if (graph[a][v] >= minWeight[v]) {
+                    } else {
+                        arrayForMST[v] = a;
+                        minWeight[v] = graph[a][v];
+                    }
+                }
+            }
+        }
+
+        //print call
+        print(arrayForMST, numVerticies, graph);
+    }
+}
